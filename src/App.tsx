9290c3e9a88ecf0246207d90a7f11b15358f1da1
Unstabel <Lupe0001@stud.kea.dk>
2024-03-14 17:25:05 +0100

    Added buttons
    Added buttons for each activity

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+    const [selectedView, setSelectedView] = useState("homepage");
+
+
+    function handleSelected(selected: string) {
+        setSelectedView(selected);
+    }
+
+
+    return (
+        <>
+            <div className="outer-div-style">
+                <div className="header-style">
+                    <h2>AdventureXP</h2>
+                </div>
+                <div style={{ display: "flex" }}>
+                    <div style={{ flex: 1, padding: 10 }}>
+                        <Buttons onSelected={handleSelected} />
+                    </div>
+                    <div className="button-style">
+                        {selectedView == "homepage" ? <p>AdventureXP Homepage</p> : null}
+                        {selectedView == "gokart" ? <p>AdventureXP Go-kart</p> : null}
+                        {selectedView == "minigolf" ? <p>AdventureXP Mini-gold</p> : null}
+                        {selectedView == "paintball" ? <p>AdventureXP Paintball</p> : null}
+                        {selectedView == "climbing" ? <p>AdventureXP Climbing</p> : null}
+                        {/* make new pages for the buttons here  */}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
-
-export default App;
+type ButtonProps = {
+    onSelected: (selected: string) => void;
+};
+const Buttons = (props: ButtonProps) => {
+    const { onSelected: handleSelected} = props;
+    return (
+        <>
+            <button className="btn-w100" onClick={() => handleSelected("homepage")}>
+                Homepage
+            </button>
+            <button className="btn-w100" onClick={() => handleSelected("gokart")}>
+                Go-kart
+            </button>
+            <button className="btn-w100" onClick={() => handleSelected("minigolf")}>
+                Mini-golf
+            </button>
+            <button className="btn-w100" onClick={() => handleSelected("paintball")}>
+                Paintball
+            </button>
+            <button className="btn-w100" onClick={() => handleSelected("climbing")}>
+                Climbing
+            </button>
+            {/* Add a new buttons here */}
+        </>
+    );
+};
